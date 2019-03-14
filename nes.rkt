ec70a949382f44cdefd437c3f24f56103c1deee7
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-03-14 11:23:13 +0000

    Add files via upload
    opcodes json by kirbyUK
    https://gist.github.com/kirbyUK/1a0797e19f54c1e35e67ce7b385b323e

---
+++ nes.rkt
@@ -0,0 +1,79 @@
+#lang rackjure
+;(require bitsyntax)
+;(require file/sha1)
+;(require binaryio)
+(require threading)
+(require csv-reading)
+(require sugar)
+(require json)
+
+(define 16KB #x4000)
+(define 8KB  #x2000)
+(define iNES-identifier (bytes #x4e #x45 #x53 #x1a))
+
+;"bytes" : "2",
+;"description" : "Add with Carry",
+;"name" : "ADC",
+;"opcode" : "$69",
+;"mode" : "Immediate"
+
+(define OPS
+  (~> (open-input-file "6502_instructions.json")
+      (read-json)))
+
+(define hex-string->number
+  (λ~> (substring 1)
+       (string->number 16)))
+
+(define OP-VEC
+  (let ([v (make-vector 256 #f)])
+    (for ([op (in-list OPS)])
+      (match-let* ([(hash-table ('opcode opcode) ('name name) ('mode mode) ('bytes bytes)) op])
+        (vector-set! v (hex-string->number opcode) (vector opcode name mode bytes))))
+    v))
+
+(for ([op OP-VEC])
+  (displayln op))
+
+
+;(string->number (substring (op 'opcode) 1) 16))
+
+;; CONSTANTS ;;
+(define 1*PRG-BANK #x4000)
+(define 1*CHR-BANK #x2000)
+
+
+
+(define (valid-ines-header? h)
+  (for/and ([b (in-bytes h)]
+            [n (in-bytes iNES-identifier)])
+    (equal? b b)))
+
+(define (filesize-matches-header? file-size
+                                  prg-size
+                                  chr-size)
+  (if (= (+ prg-size chr-size 16)
+         file-size)
+    "FILESIZE MATCHES HEADER"
+    "FILESIZE DOES NOT MATCH HEADER"))
+
+(define (main rom-path)
+  (define ROM (open-input-file rom-path #:mode 'binary))
+  (unless (valid-ines-header? (read-bytes 4 ROM))
+    (error "error: no iNES header")
+    (exit))
+  (define PRG-SIZE (* (read-byte ROM) 16KB))
+  (define CHR-SIZE (* (read-byte ROM) 8KB))
+  (unless (filesize-matches-header? (file-size rom-path)
+                                    PRG-SIZE
+                                    CHR-SIZE)
+    (error "error: filesize does not match header")
+    (exit)))
+
+
+
+
+(main "mario.nes")
+
+
+
