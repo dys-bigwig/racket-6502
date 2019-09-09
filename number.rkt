e806634d6d047b769362f6044e21ee70d3d812a3
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-09-09 17:54:59 +0100

    Update number.rkt

--- number.rkt
+++ number.rkt
@@ -1,5 +1,6 @@
-#lang racket
-(require parser-tools/lex (prefix-in : parser-tools/lex-sre))
+#lang racket/base
+(require (only-in parser-tools/lex define-lex-abbrev numeric)
+         (prefix-in : parser-tools/lex-sre))
 (provide (all-defined-out))
 
 (define-lex-abbrev hex-digit (:or numeric (char-range #\a #\f) (char-range #\A #\F)))
