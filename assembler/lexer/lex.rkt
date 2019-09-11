a7b86808d49d16ade0796234238fa15df2d0dbe0
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-09-11 03:37:39 +0100

    Add files via upload

--- assembler/lexer/lex.rkt
+++ assembler/lexer/lex.rkt
@@ -1,24 +1,31 @@
 #lang racket/base
 (require parser-tools/lex (prefix-in : parser-tools/lex-sre))
-(require "transformer.rkt" "mnemonic.rkt" "number.rkt" "reserved.rkt")
+(require "transformer.rkt" "mnemonic.rkt" "number.rkt" "reserved.rkt" "directive.rkt")
 (provide lex delimiters directives mnemonics atoms letters)
 
 (define-empty-tokens delimiters (lparen rparen lbracket dollar rbracket hashtag comma newline colon eof))
-(define-empty-tokens directives (equate db))
+(define-empty-tokens directives (equals db))
 (define-tokens mnemonics (mnemonic))
-(define-tokens atoms (int identifier))
+(define-tokens atoms (int identifier label))
 (define-empty-tokens letters (A X Y))
 
 (define-lex-abbrev identifier/l (:& (complement reserved/l)
+                                    (complement number/l)
                                     (:+ (:or alphabetic numeric))))
+
+(define-lex-abbrev label/l (:: identifier/l #\:))
 
 (define lex
   (lexer
-    [(:+ (:or #\space #\tab #\newline)) (lex input-port)]
+    [(:+ (:or #\space #\tab )) (lex input-port)]
+    [(:+ #\newline) (token-newline)]
     [#\# (token-hashtag)]
     [#\: (token-colon)]
+    [db/l (token-db)]
     [mnemonic/l (token-mnemonic lexeme)]
     [identifier/l (token-identifier lexeme)]
+    [#\= (token-equals)]
+    [label/l (token-label lexeme)]
     [(:+ numeric) (token-int (string->number lexeme))]
     [(:: #\$ (:+ hex-digit)) (token-int (string->number (substring lexeme 1) 16))]
     [#\( (token-lparen)]
